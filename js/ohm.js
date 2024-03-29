15722d54f21745f94c29407ba1d263863231fdc3
X-zeed <134188500+X-zeed@users.noreply.github.com>
2024-03-29 22:16:08 +0700

    Add files via upload

---
+++ js/ohm.js
@@ -0,0 +1,19 @@
+document.getElementById('Pn').onclick = function () {
+    alert("My Phone number = 096 - 651 - 9965");
+};
+
+document.getElementById('Discord').onclick = function () {
+    alert("My Discord = Sagame1688#1518");
+};
+
+document.getElementById('github').onclick = function() {
+    window.location.href = 'https://github.com/NameJang';
+};
+
+document.getElementById('ig').onclick = function() {
+    window.location.href = 'https://www.instagram.com/o.h.m.i.n.d.y/';
+};
+
+document.getElementById('fb').onclick = function() {
+    window.location.href = 'https://www.facebook.com/profile.php?id=100057517363644';
+};
